24b68857c76f1fd10cd093535ecfa2ad3321dbe1
AlexKri72 <krial11@rambler.ru>
2022-04-14 23:35:27 +0500

    ready task003

--- task003.py
+++ task003.py
@@ -2,9 +2,9 @@
 import os
 os.system("cls")
 
-n = int(input('Введите любое целое число: '))
-m = -n
-while m<=n:
-    print(m, end=' ')
-    m+=1
+n = abs(int(input('Введите любое целое число: ')))
+
+for i in range(-n, n+1):
+    print(i, end=' ')
+
 print('\n')
