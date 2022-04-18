90577e9a89cafcdc04d58638ed697a3128da8086
AlexKri72 <krial11@rambler.ru>
2022-04-18 23:22:01 +0500

    task 007 changed

--- task007.py
+++ task007.py
@@ -8,19 +8,13 @@ y = [True, False]
 z = [True, False]
 print('\tX\tY\tZ\t¬(X ⋁ Y ⋁ Z)\t¬X ⋀ ¬Y ⋀ ¬Z\t¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z')
 print('_'*100)
-index = 0
-while (index <= 1):
-    count = 0
-    while (count <= 1):
-        i = 0
-        while(i <= 1):
+for index in range(2):
+    for count in range(2):
+        for i in range(2):
             left = not(x[index] and y[count] and z[i])  # левая часть равенства
             print(f"\t{x[index]} \t{y[count]} \t{z[i]}\t\t{left}", end='')
             right = not(x[index]) or not(y[count]) or not(
                 z[i])  # правая часть равенства
             print(f"\t\t{right}", end='')
             print('\t\t', left == right)
-            i += 1
-        count += 1
-    index += 1
 print('\n')
