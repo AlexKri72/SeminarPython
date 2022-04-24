638a5ba9f15f01fd97bc7475765f4caed34734b6
AlexKri72 <krial11@rambler.ru>
2022-04-24 12:16:22 +0500

    task031

---
+++ task031.py
@@ -0,0 +1,15 @@
+# Составить список простых множителей натурального числа N
+import os
+from math import pi
+from random import*
+os.system("cls")
+
+n = randint(1, 1001)
+print('Заданное число = ', n)
+i = 2
+while i <= n:
+    if n % i == 0:
+        print(i)
+        n = n/i
+        i -= 1
+    i += 1
