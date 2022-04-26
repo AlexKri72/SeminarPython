3ae3863318604c972d320749677a8281b1028d7e
AlexKri72 <krial11@rambler.ru>
2022-04-26 17:04:49 +0500

    task024

--- task024.py
+++ task024.py
@@ -7,8 +7,13 @@ from random import *
 os.system("cls")
 
 list = [uniform(1, 21) for i in range(8)]
-print(list, '\n')
-for i in range(len(list)):
-    list[i] = list[i] % 10/10
 print(list)
-print('\nМаксимальное число: ', max(list), '\n')
+list1 = []
+
+for i in list:
+    if i % 1 != 0:
+        list1.append(i % 1)
+print(list1)
+print('Максимальное число: ', round(max(list1), 2))
+print('Минимальное число: ', round(min(list1), 2))
+print('Разница: ', round(max(list1) - min(list1), 2))
