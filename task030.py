5f46b9f10fd28f6a9d1ff62b92c28f6338c696f0
AlexKri72 <krial11@rambler.ru>
2022-05-09 12:50:57 +0500

    task030

--- task030.py
+++ task030.py
@@ -7,6 +7,18 @@ from random import*
 os.system("cls")
 
 d = randint(1, 10)
-print('Точность вывода = ', d)
+print('Точность вывода = ', d, 'после запятой')
 
-print('Пи = ', round(pi, d), '\n')
+a = 1.0
+for i in range(d):
+    a = a/10.0
+
+# переводим точность в вещественное число, по условию задачи
+print('Точность вывода = ', round(a, d))
+
+count = 0
+while a < 1:  # считаем сколько нужно знаков после запятой
+    a *= 10
+    count += 1
+
+print('Пи = ', round(pi, count), '\n')
