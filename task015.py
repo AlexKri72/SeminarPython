bcbdc948219c50ac8572dd07f13a52384733a94c
AlexKri72 <krial11@rambler.ru>
2022-05-16 11:39:37 +0500

    task015

--- task015.py
+++ task015.py
@@ -1,13 +1,12 @@
 # Написать программу получающую набор произведений чисел от 1 до N.
 # Пример: пусть N = 4, тогда [ 1, 2, 6, 24 ]
-from audioop import mul
 import os
 from itertools import accumulate
 import operator
 import random
 os.system("cls")
 
-n = random.randint(6, 16)
+n = random.randint(8, 16)
 
 num = list(accumulate([x for x in range(1, n + 1)], operator.mul))
 
